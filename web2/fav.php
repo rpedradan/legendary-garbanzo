f8a8754a8c94b30f146725ca1599b85282e52868
Kiefer Cervantes <90808927+kmcervantes17@users.noreply.github.com>
2021-10-19 20:21:18 +0800

    done practice 5-01

--- web2/fav.php
+++ web2/fav.php
@@ -2,7 +2,16 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
-        
+       if ($_POST['username'] && $_POST['dish']){
+            
+            $username = htmlentities ($_POST['username']);
+            $color = $_POST['color'];
+            $dish = htmlentities ($_POST['dish']);
+
+            echo "Thanks for your selection, {$username}.".'<br>'."You really enjoyed your {$dish} - especially with a nice {$color} wine.";
+        } else {
+            echo "Missing details. Please fill out the form completely.";
+        } 
     ?>
 </body>
 </html>
