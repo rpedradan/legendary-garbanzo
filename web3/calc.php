05eda92bb1d7326bf8ebb59e8f65f6dd4bcb918d
iklagan00 <201980202@psu.palawan.edu.ph>
2021-10-20 03:28:22 +0800

    done practice 5-02

--- web3/calc.php
+++ web3/calc.php
@@ -2,16 +2,30 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+        $fval = $_POST['val1'];
+        $sval = $_POST['val2'];
         # test if input is_numeric
-        if(1){
+        if(is_numeric($fval) && is_numeric($sval)){
             # test if calc is not null
-            if(1){
+            if(isset($_POST['calc'])){
+                $ops = $_POST['calc'];
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($ops){
+                    case 'add':
+                        $result = $fval + $sval;
+                        break;
+                    case 'sub':
+                        $result = $fval - $sval;
+                        break;
+                    case 'mul':
+                        $result = $fval * $sval;
+                        break;
+                    case 'div':
+                        $result = $fval / $sval;
                 }
                 echo "Calculation result: $result";
             }
+            else{ echo "No operation - please retry";}
         }
         else{ echo "Invalid entry - please retry"; }
     ?>
